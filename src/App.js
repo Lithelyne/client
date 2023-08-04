45b373704cc8e74f32ae275ab1c1cbff7beb9747
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-08-03 21:48:43 -0700

    Added footer, navbar, details, and home components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material'
 import './App.css';
 
+import { ExerciseDetail } from './pages/ExerciseDetail';
+import { Home } from './pages/Home';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+
+
 function App() {
   return (
     <Box width="400px">
@@ -11,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
