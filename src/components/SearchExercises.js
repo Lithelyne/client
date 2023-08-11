ec9968fc6fc0b706738fd46009ff04eff44d0deb
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-08-11 13:13:46 -0700

    functions created, not working yet

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,19 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 
 export const SearchExercises = () => {
+  const SearchExercises = () => {
+    const [search, setSearch] = useState('')
+    }
+
+    const handleSearch = async () => {
+        if(search) {
+          // const exercisesData = await fetchData();
+        }
+    }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-
+    
       <Typography fontWeight={700}
         sx={{ fontSize: { lg: '44px', xs: '30px' } }}
         mb="50px" textAlign="center">
@@ -15,13 +25,14 @@ export const SearchExercises = () => {
       <Box position="relative" mb="72px">
         <TextField
           sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '800px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-          height="76px" value=""
-          onChange={(e) => { }}
+          height="76px" value={search}
+          onChange={(e) => {setSearch(e.target.value.toLowerCase()) }}
           placeholder="Search Exercises" type="text">
         </TextField>
 
         <Button className="search-btn"
-          sx={{ bgcolor: '#ff2625', color: '#fff', textTransform: 'none', width: { lg: '175px', xs: '80px' }, fontSize: { lg: '20px', xs: '14px' }, height: '56px', position: "absolute", right: '0' }}>
+          sx={{ bgcolor: '#ff2625', color: '#fff', textTransform: 'none', width: { lg: '175px', xs: '80px' }, fontSize: { lg: '20px', xs: '14px' }, height: '56px', position: "absolute", right: '0' }}
+          onClick={handleSearch}>
           Search
         </Button>
       </Box>
