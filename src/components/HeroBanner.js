6313c8a580b579afeb7324e4ec4b4da729474359
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-08-11 12:45:55 -0700

    HeroBanner Completion, Search Halfwaydone

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,11 +9,13 @@ export const HeroBanner = () => {
 
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
+        
       </Typography>
 
       <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '40px'}}}
       mb="23px" mt="30px">
         Sweat, Smile <br /> and Repeat
+        
       </Typography>
 
       <Typography fontSize="22px" lineHeight="35px" mb= {4}>
@@ -29,7 +31,7 @@ export const HeroBanner = () => {
         sx={{ opacity : 0.1, display: {lg: 'block', xs: 'none'}}}>
           Exercise
         </Typography>
-        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+        {/* <img src={HeroBannerImage} alt="banner" className="hero-banner-img" /> */}
     </Box>
   )
 }
